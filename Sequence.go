3127c456ddee8ac8ac716fd9a6454f222d90fef1
Sebastia <>
2018-05-08 18:22:06 +0200

    normalized initial randomization of params

--- Sequence.go
+++ Sequence.go
@@ -1,5 +1,9 @@
 package main
 
+import ("math/rand"
+	"time"
+)
+
 type Sequence struct {
 	HiddenStates []string
 	ObservableStates []string
@@ -22,14 +26,40 @@ func CreateSequence(HiddenStates []string, ObservableStates []string) *Sequence{
 }
 
 func CreateRandomArray(numElem int) []float64{
+	rand.Seed(time.Now().UTC().UnixNano())
 	m := make([]float64,numElem)
+	var k,sum float64
+
+	for i,_ := range(m){
+		k = rand.Float64()
+		sum += k
+		m[i] = k
+	}
+	for i,_ := range(m){
+		m[i] = m[i]/sum
+	}
 	return m
 }
 
 func CreateRandomMatrix(numRows int, numCols int) [][]float64{
+	rand.Seed(time.Now().UTC().UnixNano())
 	m := make([][]float64, numRows)
+	var k,sum float64
 	for i := 0; i < numRows; i++ {
 		m[i] = make([]float64, numCols)
+	}
+
+	for i := 0; i < numRows; i++{
+		for j := 0; j< numCols; j++{
+			k = rand.Float64()
+			sum += k
+			m[i][j] = k
+		}
+	}
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < numCols; j++ {
+			m[i][j] = m[i][j]/sum
+		}
 	}
 	return m
 }
--- main.go
+++ main.go
@@ -4,22 +4,6 @@ import "fmt"
 
 
 func main() {
-
-	StartProbability := map[string]float64{"Rainy": 0.6, "Sunny": 0.4}
-	TransitionProbability := map[string]map[string]float64{"Rainy" : {"Rainy": 0.7, "Sunny": 0.3},"Sunny" : {"Rainy": 0.4, "Sunny": 0.6}}
-	EmissionProbability := map[string]map[string]float64{"Rainy" : {"walk": 0.1, "shop": 0.4, "clean": 0.5}, "Sunny" : {"walk": 0.6, "shop": 0.3, "clean": 0.1}}
-
-
-	fmt.Println(StartProbability)
-	fmt.Println(TransitionProbability)
-	fmt.Println(EmissionProbability)
-
 	seq := CreateSequence([]string{"Rainy","Sunny"},[]string{"walk","shop","clean"})
 	fmt.Println(seq)
-
-	fmt.Println(CreateRandomMatrix(4,4))
-	fmt.Println(CreateRandomArray(4))
-
-	fmt.Println(CreateRandomMatrix(4,4)[1][2])
-
 }
