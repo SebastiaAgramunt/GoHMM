7535623511775ce8c7f74439ab66f9f3dce19e24
Sebastia Agramunt <yc00113@Alpha-123519.home>
2018-11-25 20:48:41 +0100

    refactor

--- Sequence.go
+++ Sequence.go
@@ -1,56 +1,57 @@
 package main
 
-import ("math/rand"
+import (
+	"math/rand"
 	"time"
 )
 
 type Sequence struct {
-	HiddenStates []string
+	HiddenStates     []string
 	ObservableStates []string
 
-	StartProbability []float64
+	StartProbability      []float64
 	TransitionProbability [][]float64
-	EmissionProbability [][]float64
+	EmissionProbability   [][]float64
 }
 
-func CreateSequence(HiddenStates []string, ObservableStates []string) *Sequence{
+func CreateSequence(HiddenStates []string, ObservableStates []string) *Sequence {
 
 	NHidden := len(HiddenStates)
 	NObservable := len(ObservableStates)
 
-	return &Sequence{HiddenStates:HiddenStates,
-	ObservableStates:ObservableStates,
-	StartProbability: CreateRandomArray(NHidden),
-	TransitionProbability:CreateRandomMatrix(NHidden,NHidden),
-	EmissionProbability:CreateRandomMatrix(NHidden,NObservable)}
+	return &Sequence{HiddenStates: HiddenStates,
+		ObservableStates:      ObservableStates,
+		StartProbability:      CreateRandomArray(NHidden),
+		TransitionProbability: CreateRandomMatrix(NHidden, NHidden),
+		EmissionProbability:   CreateRandomMatrix(NHidden, NObservable)}
 }
 
-func CreateRandomArray(numElem int) []float64{
+func CreateRandomArray(numElem int) []float64 {
 	rand.Seed(time.Now().UTC().UnixNano())
-	m := make([]float64,numElem)
-	var k,sum float64
+	m := make([]float64, numElem)
+	var k, sum float64
 
-	for i,_ := range(m){
+	for i, _ := range m {
 		k = rand.Float64()
 		sum += k
 		m[i] = k
 	}
-	for i,_ := range(m){
-		m[i] = m[i]/sum
+	for i, _ := range m {
+		m[i] = m[i] / sum
 	}
 	return m
 }
 
-func CreateRandomMatrix(numRows int, numCols int) [][]float64{
+func CreateRandomMatrix(numRows int, numCols int) [][]float64 {
 	rand.Seed(time.Now().UTC().UnixNano())
 	m := make([][]float64, numRows)
-	var k,sum float64
+	var k, sum float64
 	for i := 0; i < numRows; i++ {
 		m[i] = make([]float64, numCols)
 	}
 
-	for i := 0; i < numRows; i++{
-		for j := 0; j< numCols; j++{
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < numCols; j++ {
 			k = rand.Float64()
 			sum += k
 			m[i][j] = k
@@ -58,7 +59,7 @@ func CreateRandomMatrix(numRows int, numCols int) [][]float64{
 	}
 	for i := 0; i < numRows; i++ {
 		for j := 0; j < numCols; j++ {
-			m[i][j] = m[i][j]/sum
+			m[i][j] = m[i][j] / sum
 		}
 	}
 	return m
